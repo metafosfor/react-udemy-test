79002a067030c22bad8d748a0a590dd1c0aa484c
metafosfor <metafosfor@gmail.com>
2026-05-06 22:55:01 +0300

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message(){
+    const name = "Dmytro";
+    return <h1>Hello , world! From {name}</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+function ListGroup() {
+    const items = [
+        "New York",
+        "San Francisco",
+        "Tokyo",
+        "London",
+        "Paris",
+        "Berlin"];
+
+    const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+    return (
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                {items.map(item => <li
+                    className={"list-group-item" + (selectedItem === item ? " active" : "")}
+                    key={item}
+                    onClick={() => setSelectedItem(item)}
+                >
+                    {item}
+                </li>)}
+
+            </ul>
+        </>
+    );
+
+}
+
+export default ListGroup;
